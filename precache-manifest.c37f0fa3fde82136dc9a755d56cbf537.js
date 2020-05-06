d456ae66f77ac39c68118e3d224d476f7da333fd
Vincent <vincentkok@live.nl>
2020-05-06 11:33:20 +0200

    Updates

---
+++ precache-manifest.c37f0fa3fde82136dc9a755d56cbf537.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25040c1f9b5c30795a4c3e02244e35e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0326dc4ad1d746c6c242",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1826f93657d36e5d4e28",
+    "url": "/static/js/2.5394b754.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5394b754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0326dc4ad1d746c6c242",
+    "url": "/static/js/main.52587625.chunk.js"
+  },
+  {
+    "revision": "b53222df98beef7a3cd1",
+    "url": "/static/js/runtime-main.1e8ecbc5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
